763883a77b0e79fefc0379355fd431e7ba27f348
imjinnotgin <imjinnotgin@gmail.com>
2025-05-13 09:57:50 +0900

    Update WeavingDraft.js

--- WeavingDraft.js
+++ WeavingDraft.js
@@ -22,15 +22,25 @@ class WeavingDraft {
   }
 
   draw() {
-    background(255);
-    noStroke();
-    for (let y = 0; y < this.gridCount; y++) {
-      for (let x = 0; x < this.gridCount; x++) {
-        if (this.grid[y][x]) {
-          fill(0);
-          rect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
-        }
+  background(255);
+  stroke(0);         // 검은색 테두리
+  strokeWeight(0.25); // 매우 얇은 선
+
+  for (let y = 0; y < this.gridCount; y++) {
+    for (let x = 0; x < this.gridCount; x++) {
+      const val = this.grid[y][x];
+
+      // 셀이 채워져 있으면 색을 채우고, 아니면 비움
+      if (val) {
+        fill(val);
+      } else {
+        noFill();
       }
+
+      // 항상 테두리를 그림
+      rect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
     }
   }
+}
+
 }
