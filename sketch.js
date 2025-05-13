69390013323369106d076fc8ea419dea132345ac
imjinnotgin <imjinnotgin@gmail.com>
2025-05-13 10:06:02 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 let weavingDraft;
 const canvasSize = 700;
 const gridCount = 48;
@@ -12,11 +11,21 @@ function setup() {
   weavingDraft = new WeavingDraft(gridCount, cellSize);
   weavingDraft.generate(random(1000), random(1000));
   weavingDraft.draw();
+  drawGridOverlay(gridCount, cellSize);
 
   initMap();
 }
 
 function draw() {}
+
+function drawGridOverlay(gridCount, cellSize) {
+  stroke(0);
+  strokeWeight(1);
+  for (let i = 0; i <= gridCount; i++) {
+    line(i * cellSize, 0, i * cellSize, canvasSize);
+    line(0, i * cellSize, canvasSize, i * cellSize);
+  }
+}
 
 function initMap() {
   const map = L.map('map').setView([37.5665, 126.9780], 3);
@@ -26,6 +35,42 @@ function initMap() {
     const lat = e.latlng.lat;
     const lng = e.latlng.lng;
     weavingDraft.generate(lat, lng);
+    redraw();
     weavingDraft.draw();
+    drawGridOverlay(gridCount, cellSize);
   });
+}
+
+class WeavingDraft {
+  constructor(gridCount, cellSize) {
+    this.gridCount = gridCount;
+    this.cellSize = cellSize;
+    this.grid = [];
+  }
+
+  generate(latSeed, lngSeed) {
+    noiseSeed(latSeed + lngSeed);
+    this.grid = [];
+    for (let y = 0; y < this.gridCount; y++) {
+      let row = [];
+      for (let x = 0; x < this.gridCount; x++) {
+        row.push(noise(x * 0.3, y * 0.3) > 0.5 ? 1 : 0);
+      }
+      this.grid.push(row);
+    }
+  }
+
+  draw() {
+    for (let y = 0; y < this.gridCount; y++) {
+      for (let x = 0; x < this.gridCount; x++) {
+        if (this.grid[y][x] === 1) {
+          fill(0);
+        } else {
+          fill(255);
+        }
+        noStroke();
+        rect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+      }
+    }
+  }
 }
